c02c2ada85af5895d10f85527f0a0938d7fe6149
Brennan Wade <brenw2@gmail.com>
2015-02-12 18:47:07 -0500

    able to rotate saturn and zoom

--- SaternProject/Saturn/saturn.js
+++ SaternProject/Saturn/saturn.js
@@ -22,6 +22,12 @@ SaturnApp.prototype.init = function(param)
     var sun = new Sun();
     sun.init();
     this.addObject(sun);
+     this.camera.position.set(0, 0, 5);
+    this.root.rotation.x = Math.PI / 6;
+    
+    this.lastX = 0;
+	this.lastY = 0;
+    this.mouseDown = false;
 }
 
 
@@ -110,8 +116,8 @@ Saturn.prototype.update = function()
 	Sim.Object.prototype.update.call(this);
 }
 
-Saturn.TILT = -0.466;
-Saturn.REVOLUTION_Y = 0.003;
+Saturn.TILT = .27;
+Saturn.REVOLUTION_Y = 0.01;
 
 // The rings
 Saturn.Rings = function ( innerRadius, outerRadius, nSegments ) {
@@ -193,3 +199,58 @@ Sun.prototype.init = function()
     // Tell the framework about our object
     this.setObject3D(light);    
 }
+
+SaturnApp.prototype.handleMouseMove = function(x, y)
+{
+	if (this.mouseDown)
+	{
+		var dx = x - this.lastX;
+		if (Math.abs(dx) > SaturnApp.MOUSE_MOVE_TOLERANCE)
+		{
+			this.root.rotation.y -= (dx * 0.01);
+		}
+		this.lastX = x;
+		
+		return;
+		
+		var dy = y - this.lastY;
+		if (Math.abs(dy) > SaturnApp.MOUSE_MOVE_TOLERANCE)
+		{
+			this.root.rotation.x += (dy * 0.01);
+			
+			// Clamp to some outer boundary values
+			if (this.root.rotation.x < 0)
+				this.root.rotation.x = 0;
+			
+			if (this.root.rotation.x > SaturnApp.MAX_ROTATION_X)
+				this.root.rotation.x = SaturnApp.MAX_ROTATION_X;
+			
+		}
+		this.lastY = y;
+		
+	}	
+}
+
+SaturnApp.prototype.handleMouseDown = function(x, y)
+{
+	this.lastX = x;
+	this.lastY = y;
+	this.mouseDown = true;
+}
+
+SaturnApp.prototype.handleMouseUp = function(x, y)
+{
+	this.lastX = x;
+	this.lastY = y;
+	this.mouseDown = false;
+}
+
+SaturnApp.prototype.handleMouseScroll = function(delta)
+{
+	var dx = delta;
+
+	this.camera.position.z -= dx;
+}
+
+SaturnApp.MOUSE_MOVE_TOLERANCE = 0;
+SaturnApp.MAX_ROTATION_X = Math.PI / 2;
